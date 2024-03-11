3f8ea1e2a186b6002982c5959f4ee6db4380e579
kota_shiratori <swan.kouta@gmail.com>
2024-03-11 23:10:18 +0900

    🎨 アプリケーションの作成

--- vue-official-documentation/src/main.js
+++ vue-official-documentation/src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// createApp 関数で新しい アプリケーションのインスタンス を作成することから始まります:
+// アプリケーションのインスタンスは .mount() メソッドが呼ばれるまで何もレンダリングしません。
+createApp(App).mount("#app");
+
+// ↓複数のアプリケーションのインスタンス
+// const app1 = createApp({
+//   /* ... */
+// });
+// app1.mount("#container-1");
+
+// const app2 = createApp({
+//   /* ... */
+// });
+// app2.mount("#container-2");
